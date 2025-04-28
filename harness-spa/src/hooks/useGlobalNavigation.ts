7ddbc4ca4d9d2f061887bb5871b312609b611e8e
Stef Stachow <stef@stachow.co.uk>
2025-04-28 14:31:53 +0100

    Header improvements

---
+++ harness-spa/src/hooks/useGlobalNavigation.ts
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const GLOBAL_EVENT_NAME = "cps-global-nav-event";
+type GlobalNavigationEvent = CustomEventInit<string>;
+
+export const useGlobalNavigation = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handler = (event: GlobalNavigationEvent) => navigate(event.detail!);
+
+    window.addEventListener(GLOBAL_EVENT_NAME, handler);
+    return () => window.removeEventListener(GLOBAL_EVENT_NAME, handler);
+  }, []);
+};
