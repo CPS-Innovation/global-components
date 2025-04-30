34c83f7e5421d90f1a7d7c56f4d6a3d8258f6ad7
stef <stef@stachow.co.uk>
2025-04-30 08:58:44 +0100

    Feature/fct2 6228 embed apps (#32)

--- harness-spa/deploy.sh
+++ harness-spa/deploy.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
+ENVIRONMENT=$1
 #VERSION=$(jq -r '.version' package.json)
 STORAGE_ACCOUNT_NAME=sacpsglobalcomponents
 CONTAINER_NAME=\$web
-VITE_GLOBAL_SCRIPT_URL=https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/dev/cps-global-components.js npm run build
+VITE_GLOBAL_SCRIPT_URL=https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/$ENVIRONMENT/cps-global-components.js npm run build
+
+if [[ "$ENVIRONMENT" = "latest" ]]; then
+    FOLDER_SUFFIX=""
+else
+    FOLDER_SUFFIX="-$ENVIRONMENT"
+fi
 
 # If using azcopy with az AD account login, the AD account needs the "Storage Blob Data Contributor" role
 export AZCOPY_AUTO_LOGIN_TYPE=AZCLI
-azcopy sync ./dist/ https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/\$web/spa-app/  --recursive=true --delete-destination=true
+azcopy sync ./dist/ https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/$CONTAINER_NAME/spa-app$FOLDER_SUFFIX/  --recursive=true --delete-destination=true
 
 # az storage blob delete-batch \
 #     --auth-mode login \
--- harness-static/deploy.sh
+++ harness-static/deploy.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 ENVIRONMENT=$1
-VERSION=$(jq -r '.version' package.json)
+#VERSION=$(jq -r '.version' package.json)
 STORAGE_ACCOUNT_NAME=sacpsglobalcomponents
 CONTAINER_NAME=\$web
-GLOBAL_SCRIPT_URL=https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/dev/cps-global-components.js npm run build
+
+if [[ "$ENVIRONMENT" = "latest" ]]; then
+    BASE_PATH="/static-app"
+else
+    BASE_PATH="/static-app-$ENVIRONMENT"
+fi
+
+GLOBAL_SCRIPT_URL=https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/$ENVIRONMENT/cps-global-components.js BASE_PATH=$BASE_PATH npm run build
 
 # If using azcopy with az AD account login, the AD account needs the "Storage Blob Data Contributor" role
 export AZCOPY_AUTO_LOGIN_TYPE=AZCLI
-azcopy sync ./out/ https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/\$web/static-app/  --recursive=true --delete-destination=true
+azcopy sync ./out/ https://$STORAGE_ACCOUNT_NAME.blob.core.windows.net/$CONTAINER_NAME$BASE_PATH/  --recursive=true --delete-destination=true
 
 # az storage blob delete-batch \
 #     --auth-mode login \
