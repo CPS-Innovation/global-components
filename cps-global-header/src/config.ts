741c41297302e83465382b6b412dfef51641be84
Stef Stachow <stef@stachow.co.uk>
2025-05-08 10:07:36 +0100

    #FCT2-6357 - added app insights

--- cps-global-header/src/config.ts
+++ cps-global-header/src/config.ts
@@ -3,5 +3,6 @@ import { Env } from "@stencil/core";
 export const SURVEY_LINK = Env.SURVEY_LINK;
 export const SHOULD_SHOW_HEADER = Env.SHOULD_SHOW_HEADER === "true";
 export const SHOULD_SHOW_MENU = Env.SHOULD_SHOW_MENU === "true";
+export const APP_INSIGHTS_KEY = Env.APP_INSIGHTS_KEY;
 
-console.debug({ SURVEY_LINK, SHOULD_SHOW_HEADER, SHOULD_SHOW_MENU });
+console.debug({ SURVEY_LINK, SHOULD_SHOW_HEADER, SHOULD_SHOW_MENU, APP_INSIGHTS_KEY });
