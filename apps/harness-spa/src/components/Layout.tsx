70526242c76f1e763bbe705db27076e773c39ca1
stef <stef@stachow.co.uk>
2025-05-21 14:12:59 +0100

    Feature/fct2 6131 pnpm monorepo 2 (#82)
    #FCT2-6131 - added dedicated cps-global-types package (imperfect, can't figure out how to import global typings in host app without an explicit 'import' somewhere)

--- apps/harness-spa/src/components/Layout.tsx
+++ apps/harness-spa/src/components/Layout.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "cps-global-types";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
