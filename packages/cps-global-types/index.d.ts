97fe5f4acf48f6dd4f321afe9c73836a0cceeb3e
Stef Stachow <stef@stachow.co.uk>
2025-05-21 13:54:14 +0100

    #FCT2-6131 - added dedicated cps-global-types package (imperfect, can't figure out how to import global typings in host app without an explicit 'import' somewhere)

---
+++ packages/cps-global-types/index.d.ts
@@ -0,0 +1,25 @@
+// import "react";
+
+// declare global {
+//   namespace React {
+//     namespace JSX {
+//       interface IntrinsicElements {
+//         "cps-global-header": React.DetailedHTMLProps<
+//           React.HTMLAttributes<HTMLElement>,
+//           HTMLElement
+//         >;
+//       }
+//     }
+//   }
+// }
+
+declare global {
+  namespace React {
+    namespace JSX {
+      interface IntrinsicElements {
+        "cps-global-header": Partial<HTMLElementTagNameMap>;
+      }
+    }
+  }
+}
+export {};
