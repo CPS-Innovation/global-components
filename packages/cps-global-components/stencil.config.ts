ed0734461b4cfb7243d28606d574d3a671ea03bb
Stef Stachow <stef@stachow.co.uk>
2025-05-28 10:43:22 +0100

    #FCT2-6205 - silenced sass govuk deprecation warnings

--- packages/cps-global-components/stencil.config.ts
+++ packages/cps-global-components/stencil.config.ts
@@ -27,7 +27,17 @@ export const config: Config = {
   testing: {
     browserHeadless: "shell",
   },
-  plugins: [sass()],
+  plugins: [
+    sass({
+      // Silence the annoying deprecation warnings from the govuk-frontend scss.
+      //  However the deprecations are there for a reason, so if we or they upgrade
+      //  then we may have to rework some of our code.
+      //  In particular, I couldn't get @use working (instead of deprecated @import)
+      //  although I don't know enough in this area to know if it is possible.
+      quietDeps: true,
+      silenceDeprecations: ["mixed-decls", "slash-div", "import"],
+    }),
+  ],
   devServer: {
     // 3333 is the default, but lets set it explicitly as we have references to this from the other apps
     port: 3333,
