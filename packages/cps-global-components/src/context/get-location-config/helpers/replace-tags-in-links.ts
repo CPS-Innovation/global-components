7f047e7fa77c9335644bbb4f139f4970bbb2a0f7
Stef Stachow <stef@stachow.co.uk>
2025-05-29 22:49:33 +0100

    #FCT2-6995 - fixed link buttons

--- packages/cps-global-components/src/context/get-location-config/helpers/replace-tags-in-links.ts
+++ packages/cps-global-components/src/context/get-location-config/helpers/replace-tags-in-links.ts
@@ -1,7 +1,12 @@
-import { MatchedPathMatcher, OnwardLinkDefinitions } from "../../LocationConfig";
+import { MatchedPathMatcher, OnwardLinkDefinition, OnwardLinkDefinitions } from "../../LocationConfig";
 
-const replaceTags = (source: string, pathTags: MatchedPathMatcher["pathTags"]) =>
+const replaceTagsInString = (source: string, pathTags: MatchedPathMatcher["pathTags"]) =>
   Object.keys(pathTags).reduce((acc, curr) => acc.replace(new RegExp(`{${curr}}`, "g"), pathTags[curr]), source);
+
+const replaceTags = (onwardLinkDefinition: OnwardLinkDefinition, pathTags: MatchedPathMatcher["pathTags"]) =>
+  typeof onwardLinkDefinition === "string"
+    ? replaceTagsInString(onwardLinkDefinition, pathTags)
+    : { ...onwardLinkDefinition, link: replaceTagsInString(onwardLinkDefinition.link, pathTags) };
 
 export const replaceTagsInLinks = (linkDefinitions: OnwardLinkDefinitions, pathTags: MatchedPathMatcher["pathTags"]) => {
   if (!pathTags || !Object.keys(pathTags).length) {
