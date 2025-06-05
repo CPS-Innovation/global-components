8778c1c04cecda4567a6892dcab3bb6b7865fdec
Stef Stachow <stef@stachow.co.uk>
2025-06-05 20:53:50 +0100

    #FCT2-6205 - config fixes

---
+++ scripts/run-spa-harness.sh
@@ -0,0 +1,5 @@
+pnpm --filter cps-global-configuration build
+pnpm --filter cps-global-components build 
+pnpm --filter cps-global-components rollup --intro 'window.cps_global_components_build = window.cps_global_components_build || {Sha: "local", RunId: 0};' 
+cp -r ./packages/cps-global-components/dist/cps-global-components.js ./apps/harness-spa/public/assets/ 
+pnpm --filter harness-spa dev
