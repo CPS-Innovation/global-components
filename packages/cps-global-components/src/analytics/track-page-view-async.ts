07cb11eb2102feb75718b28b295e18ef78e34aa3
stef <stef@stachow.co.uk>
2025-06-09 10:38:38 +0100

    #FCT2-6283 - added puppeteer tests: tweaks (#124)

--- packages/cps-global-components/src/analytics/track-page-view-async.ts
+++ packages/cps-global-components/src/analytics/track-page-view-async.ts
@@ -4,7 +4,7 @@ declare global {
   interface Window {
     cps_global_components_build?: // title case properties as we put these values straight
     // into analytics and that is the convention there
-    { Branch: string; Sha: string; RunId: number };
+    { Branch: string; Sha: string; RunId: number; Timestamp: string };
   }
 }
 export const trackPageViewAsync = async () => {
--- scripts/run-html-harness.sh
+++ scripts/run-html-harness.sh
@@ -1,6 +1,6 @@
 pnpm --filter cps-global-configuration build
 pnpm --filter cps-global-components build 
-pnpm --filter cps-global-components rollup --intro 'window.cps_global_components_build = window.cps_global_components_build || {Sha: "local", RunId: 0};' 
+pnpm --filter cps-global-components rollup --intro 'window.cps_global_components_build = window.cps_global_components_build || {Sha: "local", RunId: 0, Timestamp: "2000-01-01T00:00:00Z"};' 
 cp -r ./packages/cps-global-components/dist/cps-global-components.js ./apps/harness-html/src/assets/ 
 cp ./configuration/config.accessibility.json ./apps/harness-html/src/assets/config.json 
 pnpm --filter harness-html start
