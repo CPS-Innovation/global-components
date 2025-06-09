304344337fb332f5bab8a7e0efc01498f3d7c312
Stef Stachow <stef@stachow.co.uk>
2025-06-09 09:12:27 +0100

    #FCT2-6283 - added puppeteer tests: try run in ci-cd

--- e2e/jest-puppeteer.config.ts
+++ e2e/jest-puppeteer.config.ts
@@ -6,9 +6,9 @@ export default {
   launch: {
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath:
-      process.env.PUPPETEER_EXEC_PATH ||
-      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
+    // executablePath:
+    //   process.env.PUPPETEER_EXEC_PATH ||
+    //   "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
   },
   browserContext: "default",
 };
