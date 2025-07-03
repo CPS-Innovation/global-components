6e2e65e0d7dfa541e3d0abf7de37d226203a6a9d
stef <stef@stachow.co.uk>
2025-07-03 08:45:12 +0100

    #FCT2-7367 - fix build artifact path problems (#157)

---
+++ to-deploy/handover.d.ts
@@ -0,0 +1,10 @@
+export declare const createOutboundUrl: ({ handoverUrl, targetUrl, }: {
+    handoverUrl: string;
+    targetUrl: string;
+}) => string;
+export declare const handleRedirect: ({ currentUrl, cookieHandoverUrl, tokenHandoverUrl, }: {
+    currentUrl: string;
+    cookieHandoverUrl: string;
+    tokenHandoverUrl: string;
+}) => string;
+//# sourceMappingURL=handover.d.ts.map
