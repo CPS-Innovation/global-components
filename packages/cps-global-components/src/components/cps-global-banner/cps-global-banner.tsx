aec408b067852b14163ae64ae116cda05d49f114
Stef Stachow <stef@stachow.co.uk>
2025-07-03 21:52:54 +0100

    #FCT2-7945 - add first-pass functionality to allow testing

--- packages/cps-global-components/src/components/cps-global-banner/cps-global-banner.tsx
+++ packages/cps-global-components/src/components/cps-global-banner/cps-global-banner.tsx
@@ -1,4 +1,5 @@
 import { Component, h } from "@stencil/core";
+import { detectOverrideMode } from "../../override-mode/detect-override-mode";
 
 @Component({
   tag: "cps-global-banner",
@@ -6,6 +7,14 @@ import { Component, h } from "@stencil/core";
   shadow: true,
 })
 export class CpsGlobalBanner {
+  private handleTitleClick = () => {
+    if (!detectOverrideMode(window)) {
+      return;
+    }
+    const currentBg = window.document.body.style.backgroundColor;
+    window.document.body.style.backgroundColor = currentBg === "lightgrey" ? "" : "lightgrey";
+  };
+
   render() {
     return (
       <div>
@@ -16,7 +25,10 @@ export class CpsGlobalBanner {
           <div class="govuk-header__container">
             <div class="govuk-header__logo">
               <span class="govuk-header__link govuk-header__link--homepage">
-                <span class="header-title">CPS</span> <span class="header-sub-title"></span>
+                <span class="header-title" onClick={this.handleTitleClick}>
+                  CPS
+                </span>{" "}
+                <span class="header-sub-title"></span>
               </span>
             </div>
           </div>
