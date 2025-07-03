a8fb4ffef94079b96c43ab151bd07f16259515a6
Stef Stachow <stef@stachow.co.uk>
2025-07-03 09:14:23 +0100

    #FCT2-7367 - tidy away junk files

--- build-artifact/auth-handover.d.ts
+++
@@ -1,8 +0,0 @@
-declare global {
-    interface Window {
-        cps_global_components_cookie_handover_url: string;
-        cps_global_components_token_handover_url: string;
-    }
-}
-export {};
-//# sourceMappingURL=auth-handover.d.ts.map
