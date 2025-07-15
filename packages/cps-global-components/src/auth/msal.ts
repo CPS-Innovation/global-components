ecbff87e04066b4a6351715343a2796f1716f459
Stef Stachow <stef@stachow.co.uk>
2025-07-15 21:41:20 +0100

    #FCT2-8173 - add test msal code

--- packages/cps-global-components/src/auth/msal.ts
+++ packages/cps-global-components/src/auth/msal.ts
@@ -22,21 +22,21 @@ export const msal = async (tenantId: string, clientId: string) => {
 
     const accounts = instance.getAllAccounts();
     console.log(accounts);
-    if (accounts.length > 0) {
-      instance.setActiveAccount(accounts[0]);
+    // if (accounts.length > 0) {
+    //   instance.setActiveAccount(accounts[0]);
 
-      // Try to silently acquire token for your API
-      const tokenRequest = {
-        scopes: ["https://graph.microsoft.com/User.Read"],
-        account: accounts[0],
-        forceRefresh: false,
-      };
+    //   // Try to silently acquire token for your API
+    //   const tokenRequest = {
+    //     scopes: ["https://graph.microsoft.com/User.Read"],
+    //     account: accounts[0],
+    //     forceRefresh: false,
+    //   };
 
-      const response = await instance.acquireTokenSilent(tokenRequest);
-      console.log(response);
-    } else {
-      console.log("No accounts");
-    }
+    //   const response = await instance.acquireTokenSilent(tokenRequest);
+    //   console.log(response);
+    // } else {
+    //   console.log("No accounts");
+    // }
   } catch (err) {
     // Silent auth failed - could be consent needed, expired tokens, etc.
     console.log("Silent auth failed:", err);
