5066a67df491965854ad389034efeec2b00bf3f0
stef <stef@stachow.co.uk>
2025-08-04 10:53:51 +0100

    #FCT2-8615 - add OS WMA and CR login reconcilliation code (#241)

--- packages/cps-global-components/src/global-script.ts
+++ packages/cps-global-components/src/global-script.ts
@@ -1,21 +1,25 @@
 //import { msal } from "./auth/msal";
 import { initialiseConfig } from "./config/config-async";
 import { detectOverrideMode } from "./override-mode/detect-override-mode";
-import { trySetupOutSystemsShim } from "./override-mode/outsystems-shim/try-setup-outsystems-shim";
-import { tryHandleOverrideSetMode } from "./override-mode/try-handle-override-set-mode";
-import { trySetupOverrideMode } from "./override-mode/try-setup-override-mode";
-
+import { setupOutSystemsShim } from "./override-mode/outsystems-shim/setup-outsystems-shim";
+import { handleOverrideSetMode } from "./override-mode/handle-override-set-mode";
+import { setupOverrideMode } from "./override-mode/setup-override-mode";
+import { isOSAuthMisaligned, createOutboundUrl } from "cps-global-os-handover";
+import { isOutSystemsApp } from "./helpers/is-outsystems-app";
 export default async () => {
-  tryHandleOverrideSetMode();
+  handleOverrideSetMode();
 
   const isOverrideMode = detectOverrideMode(window);
-  /*const configPromise = */ initialiseConfig(isOverrideMode); // no need to await, we're just optimising by kicking this off asap
+  const configPromise = initialiseConfig(isOverrideMode);
   if (isOverrideMode) {
-    trySetupOutSystemsShim(window);
-    trySetupOverrideMode(window);
-    // const { AD_TENANT_ID, AD_CLIENT_ID } = await configPromise;
-    // if (AD_TENANT_ID && AD_CLIENT_ID) {
-    //   await msal(AD_TENANT_ID, AD_CLIENT_ID);
-    // }
+    setupOutSystemsShim(window);
+    setupOverrideMode(window);
+
+    const isAuthRealignmentRequired = isOutSystemsApp(window.location.href) && isOSAuthMisaligned();
+    if (isAuthRealignmentRequired) {
+      const { OS_HANDOVER_URL } = await configPromise;
+      const redirectUrl = createOutboundUrl({ handoverUrl: OS_HANDOVER_URL, targetUrl: window.location.href });
+      console.log(`I would like to redirect to ${redirectUrl}`);
+    }
   }
 };
