6ab8b0649282f207e99dc66135b90c5a8b4e7a5b
stef <stef@stachow.co.uk>
2025-09-25 14:24:15 +0100

    #FCT2-9806 - ability to show and/or hide OutSystems headers (#307)

--- packages/cps-global-components/src/services/dom/initialise-dom-observation.ts
+++ packages/cps-global-components/src/services/dom/initialise-dom-observation.ts
@@ -1,3 +1,4 @@
+import { _console } from "../../logging/_console";
 import { FoundContext } from "../context/find-context";
 import { DomMutationObserver } from "./DomMutationSubscriber";
 import "arrive";
@@ -8,10 +9,16 @@ export const initialiseDomObservation = ({ window: { document } }: { window: Win
       const { isActiveForContext, subscriptions } = subscriber({ context });
       subscriptions.forEach(({ cssSelector, handler, unbind }) => {
         if (isActiveForContext) {
+          _console.debug("Dom observation", `Activating for ${cssSelector}`);
+          // Look for any pre-existing elements that match our criteria...
+          document.querySelectorAll(cssSelector).forEach(element => handler(element));
+          // ... then set up our mutation observation
           document.arrive(cssSelector, handler);
+          _console.debug("Dom observation", `Activated for ${cssSelector}`);
         } else {
           document.unbindArrive(cssSelector);
           unbind?.();
+          _console.debug("Dom observation", `Deactivated for ${cssSelector}`);
         }
       });
     });
