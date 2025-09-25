e9e9dc78be79331e5a91c83957fe92626e9da2b3
Stef Stachow <stef@stachow.co.uk>
2025-09-25 11:16:55 +0100

    #FCT2-9806 - ability to show and/or hide OutSystems headers

--- packages/cps-global-components/src/services/outsystems-shim/outsystems-shim-subscriber.ts
+++ packages/cps-global-components/src/services/outsystems-shim/outsystems-shim-subscriber.ts
@@ -1,38 +1,56 @@
 import { _console } from "../../logging/_console";
 import { DomMutationObserver } from "../dom/DomMutationSubscriber";
 
+type Styles = { [K in keyof CSSStyleDeclaration]?: string };
+
+const applyStyles = (styles: Styles, element: HTMLElement) =>
+  Object.entries(styles).forEach(([key, val]) => {
+    if (element[key] !== val) {
+      _console.debug("OutSystems shim", `Applying ${key}=${val} to`, element);
+      element[key] = val;
+    }
+  });
+
 export const outSystemsShimSubscriber =
+  (): DomMutationObserver =>
+  ({ context }) => ({
+    isActiveForContext: context.found && !!context.applyOutSystemsShim,
+    subscriptions: [
+      {
+        cssSelector: "header:not(cps-global-header header):not([class*='tabs']), .b1-BlueLine",
+        handler: (element: HTMLElement) => applyStyles({ display: "none" }, element),
+      },
+      {
+        cssSelector: ".main-content.ThemeGrid_Container.blue-line",
+        handler: (element: HTMLElement) => applyStyles({ top: "0", flex: "0" }, element),
+      },
+    ],
+  });
+
+export const outSystemsShimSubscriberPreviousGeneration =
   ({ window: { document } }: { window: Window }): DomMutationObserver =>
   ({ context }) => ({
     isActiveForContext: context.found && !!context.applyOutSystemsShim,
     subscriptions: [
       {
-        cssSelector: [
-          // the OS header ()
-          "header:not(cps-global-header header):not([class*='tabs'])",
-          "b1-BlueLine",
-          "b1-PlaceholderNavigation",
-        ].join(","),
-        handler: (element: HTMLElement) => {
-          _console.debug("OutSystems shim", "hiding", element);
-          if (element && element.style.display !== "none") {
-            element.style.display = "none";
-          }
-        },
+        cssSelector: ["header:not(cps-global-header header):not([class*='tabs'])", ".b1-BlueLine", ".b1-PlaceholderNavigation"].join(","),
+        handler: (element: HTMLElement) => applyStyles({ display: "none" }, element),
       },
       {
         cssSelector: "div[role='main']:not(:has(cps-global-header))",
         handler: (element: HTMLElement) => {
           const cpsHeader: HTMLCpsGlobalHeaderElement = document.createElement("cps-global-header");
-          (cpsHeader as HTMLElement).style.cssText = "top: -50px; position: relative; margin-bottom: 20px;";
-          element.insertBefore(cpsHeader as HTMLElement, element.firstChild);
+          applyStyles({ top: "-50px", position: "relative", marginBottom: "20px" }, cpsHeader);
+
+          element.insertBefore(cpsHeader, element.firstChild);
+
           _console.debug("OutSystems shim", "inserting our header");
-          // Check all ancestors and remove position: sticky
+
           let ancestor = cpsHeader.parentElement;
           while (ancestor) {
             const computedStyle = window.getComputedStyle(ancestor);
             if (computedStyle.position === "sticky") {
-              ancestor.style.position = "relative";
+              applyStyles({ position: "relative" }, ancestor);
             }
             ancestor = ancestor.parentElement;
           }
