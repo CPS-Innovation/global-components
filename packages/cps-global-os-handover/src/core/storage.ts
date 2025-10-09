9525d13ffdbc10475853ab927424b5062ba7507f
stef <stef@stachow.co.uk>
2025-10-09 11:08:00 +0100

    #FCT2-10245 - add home page to list of pre-populated CMS client vars (#337)

--- packages/cps-global-os-handover/src/core/storage.ts
+++ packages/cps-global-os-handover/src/core/storage.ts
@@ -5,6 +5,8 @@ const localStorageKeys = {
   WMA_COOKIES: "$OS_Users$WorkManagementApp$ClientVars$Cookies",
   CASE_REVIEW_JSON: "$OS_Users$CaseReview$ClientVars$CmsAuthValues",
   CASE_REVIEW_COOKIES: "$OS_Users$CaseReview$ClientVars$Cookies",
+  HOME_JSON: "$OS_Users$Casework_Blocks$ClientVars$JSONString",
+  HOME_COOKIES: "$OS_Users$Casework_Blocks$ClientVars$Cookies",
 };
 
 export const storeAuth = (cookies: string, token: string) => {
@@ -16,13 +18,16 @@ export const storeAuth = (cookies: string, token: string) => {
 
   localStorage[localStorageKeys.WMA_COOKIES] = cookies;
   localStorage[localStorageKeys.CASE_REVIEW_COOKIES] = cookies;
+  localStorage[localStorageKeys.HOME_COOKIES] = cookies;
   localStorage[localStorageKeys.WMA_JSON] = cmsAuthValuesJson;
   localStorage[localStorageKeys.CASE_REVIEW_JSON] = cmsAuthValuesJson;
+  localStorage[localStorageKeys.HOME_JSON] = cmsAuthValuesJson;
 };
 
 export const isStoredAuthCurrent = (cookies: string) =>
   areAllCookieStringsEqual(
     cookies,
     localStorage[localStorageKeys.WMA_COOKIES],
-    localStorage[localStorageKeys.CASE_REVIEW_COOKIES]
+    localStorage[localStorageKeys.CASE_REVIEW_COOKIES],
+    localStorage[localStorageKeys.HOME_COOKIES]
   );
