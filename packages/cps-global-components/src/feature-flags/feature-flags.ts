541c2f5248f66fe2ae78df7eaa2359e223e20f01
stef <stef@stachow.co.uk>
2025-10-16 14:29:46 +0100

    #FCT2-10397 - added ad-hoc user list to feature flag (#345)

--- packages/cps-global-components/src/feature-flags/feature-flags.ts
+++ packages/cps-global-components/src/feature-flags/feature-flags.ts
@@ -1,24 +1,31 @@
 import { withLogging } from "../logging/with-logging";
 import { KnownState } from "../store/store";
+import { isUserInFeatureGroup } from "./is-user-in-feature-group";
 
 const shouldEnableAccessibilityMode = ({ flags }: Pick<KnownState, "flags">) => flags.isOverrideMode;
 
 const shouldShowGovUkRebrand = ({ config }: Pick<KnownState, "config">) => !!config.SHOW_GOVUK_REBRAND;
 
 const shouldShowMenu = ({ config, auth, context }: Pick<KnownState, "config" | "auth" | "context">) => {
-  if (context.found && context.showMenuOverride === "never-show-menu") {
-    // Work management always need the menu to never appear on some pages
-    return false;
+  if (context.found) {
+    if (context.showMenuOverride === "never-show-menu") {
+      // Work management always need the menu to never appear on some pages
+      return false;
+    }
+
+    if (context.showMenuOverride === "always-show-menu") {
+      // Work management always need the menu on some pages
+      return true;
+    }
   }
 
-  if (context.found && context.showMenuOverride === "always-show-menu") {
-    // Work management always need the menu on some pages
-    return true;
-  }
+  // Note: at this point we are saying it is acceptable to be executing this code without
+  //  a context i.e. context.found === false. This is subject to being reassessed because
+  //  the menuConfig code is a related concern and a refactor may be warranted.
 
   return (
     // standard feature flag
-    !!config.SHOW_MENU && !!config.FEATURE_FLAG_ENABLE_MENU_GROUP && auth.isAuthed && auth.groups.includes(config.FEATURE_FLAG_ENABLE_MENU_GROUP)
+    !!config.SHOW_MENU && isUserInFeatureGroup({ auth, config }, "FEATURE_FLAG_MENU_USERS")
   );
 };
 
