254721612ce4c3c23e9e2e55a59d03cd0eeaf8d1
Stef Stachow <stef@stachow.co.uk>
2025-11-03 09:45:35 +0000

    #FCT2-10733 - added style based css overide to context schema

--- packages/cps-global-components/src/components/cps-global-header/cps-global-header.tsx
+++ packages/cps-global-components/src/components/cps-global-header/cps-global-header.tsx
@@ -26,8 +26,10 @@ export class CpsGlobalHeader {
   render() {
     const { isReady, state } = readyState("context");
 
+    const { headerCustomCssClasses } = isReady && state?.context.found ? state.context : { headerCustomCssClasses: undefined };
+
     return (
-      <Host class={isReady && state?.context.found ? state.context.headerCustomCssClasses : ""}>
+      <Host class={headerCustomCssClasses} style={{ foo: "bar" }}>
         <div data-internal-root data-initialisation-status={state.initialisationStatus}>
           <cps-global-banner></cps-global-banner>
           {state.fatalInitialisationError ? renderError(state.fatalInitialisationError) : <cps-global-menu></cps-global-menu>}
