9f736fad08fb3387278159e57fdb30f37357108a
Stef Stachow <stef@stachow.co.uk>
2025-12-04 18:05:11 +0000

    #FCT2-11833 - add cmsSessionHint to show/hide menu logic

--- packages/cps-global-components/src/services/data/get-case-details-factory.ts
+++ packages/cps-global-components/src/services/data/get-case-details-factory.ts
@@ -1,6 +1,6 @@
 import { validateCaseDetailsResponse } from "./validate-case-details-response";
 
 export const getCaseDetailsFactory = (fetchFn: typeof fetch) => async (caseId: string) => {
-  const response = await fetchFn(`/cases/${caseId}/summary`);
+  const response = await fetchFn(`/api/global-components/cases/${caseId}/summary`);
   return validateCaseDetailsResponse(response);
 };
