b3f8e22a715fe5318f5916475209d8e010601950
Stef Stachow <stef@stachow.co.uk>
2025-12-17 15:07:25 +0000

    Reapply "#FCT2-13186 - show menu only in proxied-CMS scenarios (#492)"
    This reverts commit 43c85ea852f9550923534cab0cda9ea0b0b4674d.

--- packages/cps-global-components/src/feature-flags/feature-flags.ts
+++ packages/cps-global-components/src/feature-flags/feature-flags.ts
@@ -7,11 +7,10 @@ const shouldEnableAccessibilityMode = ({ flags }: Pick<State, "flags">) => flags
 const shouldShowGovUkRebrand = ({ config }: Pick<State, "config">) => !!config.SHOW_GOVUK_REBRAND;
 
 const shouldShowMenu = ({ config, auth, context, cmsSessionHint }: Pick<State, "config" | "context" | "cmsSessionHint"> & { auth: State["auth"] | undefined }) => {
-  if (cmsSessionHint.found && !cmsSessionHint.result.isProxySession) {
+  if (!cmsSessionHint?.result?.isProxySession) {
     // Currently we only want the menu shown if we are connected to proxied CMS.
-    // Design decision: if cmsSessionHint was not obtained then we continue to further
-    //  logic i.e. fail-open. So if we are having problems with the hint then we will
-    //  be optimistic and show the menu.
+    // Design decision: if cmsSessionHint was not obtained then we fail-safe as
+    //  we only want to show the menu in proxied-CMS scenarios.
     return false;
   }
 
