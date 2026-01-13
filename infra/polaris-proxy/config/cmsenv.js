6ec714aff68d00d52c16e0bbcb9820d08a6b8e77
Stef Stachow <stef@stachow.co.uk>
2026-01-13 13:08:34 +0000

    #FCT2-13548 - refactored remaining cmsenv logic

--- infra/polaris-proxy/config/cmsenv.js
+++ infra/polaris-proxy/config/cmsenv.js
@@ -1,8 +1,21 @@
-﻿const _applyGlobalReplacements = (source, replacements) =>
-  replacements.reduce(
-    (prev, curr) => (prev = prev.replace(new RegExp(curr[0], "g"), curr[1])),
-    source
+﻿const _filterBody = (replacementGetters) => (r, data, flags) => {
+  // If a 302 has been issued then there's no point in processing in the response body
+  if (r.status === 302) {
+    r.sendBuffer(data, flags);
+    return;
+  }
+  const replacements = replacementGetters.reduce(
+    (acc, curr) => acc.concat(curr(r, data, flags)),
+    []
   );
+
+  data = replacements.reduce(
+    (prev, curr) => (prev = prev.replace(new RegExp(curr[0], "g"), curr[1])),
+    data
+  );
+
+  r.sendBuffer(data, flags);
+};
 
 const _parseFormData = (body) =>
   (body || "").split("&").reduce((acc, pair) => {
@@ -13,34 +26,24 @@ const _parseFormData = (body) =>
     return acc;
   }, {});
 
-const _getDomainFromCookie = (r) =>
-  (r.headersIn.Cookie || "").match(/([a-z0-9]+)\.cps\.gov\.uk/)[0];
-
-const _getCmsEnv = (r) => {
+const _getUpstreamVariable = (variableSuffix) => (r) => {
   const cookie = r.headersIn.Cookie;
-  if (!cookie || cookie.includes("cin3")) return "default";
-  if (cookie.includes("cin2")) return "cin2";
-  if (cookie.includes("cin4")) return "cin4";
-  if (cookie.includes("cin5")) return "cin5";
-  return "default";
+  const env = !cookie
+    ? "default"
+    : cookie.includes("cin3")
+    ? "default"
+    : cookie.includes("cin2")
+    ? "cin2"
+    : cookie.includes("cin4")
+    ? "cin4"
+    : cookie.includes("cin5")
+    ? "cin5"
+    : "default";
+  return r.variables[env + variableSuffix];
 };
 
-const _getUpstreamVariable = (variableSuffix) => (r) =>
-  r.variables[_getCmsEnv(r) + variableSuffix];
-
-const _getProxyDestination = (variableSuffix) => (r) =>
-  `${r.variables.endpointHttpProtocol}://${_getUpstreamVariable(variableSuffix)(
-    r
-  )}`;
-
-const _replaceCmsDomains = (hostVariableName) => (r, data, flags) => {
-  // If a 302 has been issued then there's no point in processing in the response body
-  if (r.status === 302) {
-    r.sendBuffer(data, flags);
-    return;
-  }
-
-  const replacements = [
+const _cmsDomainReplacements = (hostVariableName) => (r, data, flags) =>
+  [
     "UpstreamCmsModernDomainName",
     "UpstreamCmsServicesDomainName",
     "UpstreamCmsDomainName",
@@ -53,30 +56,23 @@ const _replaceCmsDomains = (hostVariableName) => (r, data, flags) => {
       // FCT2-13548: Not sure about this. The logic for a long time has been to
       // strip -=./ chars from the incoming variables. There have not been any
       // problems so far so keeping this in place.
-      _applyGlobalReplacements(_getUpstreamVariable(suffix)(r), [
-        ["[-=./]", ""],
-      ])
+      _getUpstreamVariable(suffix)(r).replace(new RegExp("[-=./]", "g"), "")
     )
     .map((cleanedVariable) => [cleanedVariable, r.variables[hostVariableName]]);
 
-  r.sendBuffer(_applyGlobalReplacements(data, replacements), flags);
-};
-
-const _addAppButtons = (data) =>
-  _applyGlobalReplacements(data, [
-    [
-      "objMainWindow.top.frameData.objMasterWindow.top.frameServerJS.POLARIS_URL",
-      '"/polaris"',
-    ],
-    [
-      "MENU_BAR_POLARIS_LOGO",
-      '"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAJKSURBVEhLtZXPS9tgGMe/6Q9r01Y7pyiWlWFRmTpXNybOy2AIO6gTBwpWYV4Ezz3sH1AP/gPuphdF1It49bLD8FAnMoa0MBXGJrYeiiFNbU2b+OTHac2yRNYPNHngffJ++zzfJ28YmUAVcej3qmFawfr6d8iyBIYBampcKJeBqaluiGIZbrdTzzLHVIBlP+LmpqjGLpcPXq8DR0dxrK5+xcDAQ4yNvVDXzDBtUTAYoGu9EqGhoQ7NzUESOMfgYAjb2yns75/h5CSt5v4Nyx4wjIxi0UFVsNjZSSIWe47Fxc9oa2tEMnmlZ1Vi2qLW1gVcXooUMQgEnJidjSKXKyGd5mktiJ6eehwe/sTGxgftAQMsV8DzBczPv6L+v8fISCdt/Fv999FohPz4pGdVYmtMeV4zfG7uJdrbH2Bv7weamnxYWnqL21saMQPu9R44nQxKJRkcJ9KUichkBEiScadtCfj9HvW+ufmNNs1hZqYb2WweKytfUFvrUtf+xKbJz5DPSzg9zaKjoxGRiB+pVAZrazHtAQMsV8CybuzunmF0tBPhcB36+0M4OEhjefkdEolfelYllgWUOj0eiXqex/j4E9r8HJOTT3FxwZHYIz2rEssCDB1IHCfQWD7G8fEVhobCmJ7uRV9fSM8wxrIHXi8Qj7+mt/cNBKEIn08z/F9YrkAZx4mJLjW2urmCrTEtFEp6ZB1bAsp3wS6mAtfXAl15/Zej09T4ODDD1OStrQQZqn3RJEnC8HAvWlr8+qo1TAX+B7Y8uA9VFgDuAGLN1z00rPbxAAAAAElFTkSuQmCC"',
-    ],
-  ]);
+const _appButtonReplacements = (r, data, flags) => [
+  [
+    "objMainWindow.top.frameData.objMasterWindow.top.frameServerJS.POLARIS_URL",
+    '"/polaris"',
+  ],
+  [
+    "MENU_BAR_POLARIS_LOGO",
+    '"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAAEnQAABJ0Ad5mH3gAAAJKSURBVEhLtZXPS9tgGMe/6Q9r01Y7pyiWlWFRmTpXNybOy2AIO6gTBwpWYV4Ezz3sH1AP/gPuphdF1It49bLD8FAnMoa0MBXGJrYeiiFNbU2b+OTHac2yRNYPNHngffJ++zzfJ28YmUAVcej3qmFawfr6d8iyBIYBampcKJeBqaluiGIZbrdTzzLHVIBlP+LmpqjGLpcPXq8DR0dxrK5+xcDAQ4yNvVDXzDBtUTAYoGu9EqGhoQ7NzUESOMfgYAjb2yns75/h5CSt5v4Nyx4wjIxi0UFVsNjZSSIWe47Fxc9oa2tEMnmlZ1Vi2qLW1gVcXooUMQgEnJidjSKXKyGd5mktiJ6eehwe/sTGxgftAQMsV8DzBczPv6L+v8fISCdt/Fv999FohPz4pGdVYmtMeV4zfG7uJdrbH2Bv7weamnxYWnqL21saMQPu9R44nQxKJRkcJ9KUichkBEiScadtCfj9HvW+ufmNNs1hZqYb2WweKytfUFvrUtf+xKbJz5DPSzg9zaKjoxGRiB+pVAZrazHtAQMsV8CybuzunmF0tBPhcB36+0M4OEhjefkdEolfelYllgWUOj0eiXqex/j4E9r8HJOTT3FxwZHYIz2rEssCDB1IHCfQWD7G8fEVhobCmJ7uRV9fSM8wxrIHXi8Qj7+mt/cNBKEIn08z/F9YrkAZx4mJLjW2urmCrTEtFEp6ZB1bAsp3wS6mAtfXAl15/Zej09T4ODDD1OStrQQZqn3RJEnC8HAvWlr8+qo1TAX+B7Y8uA9VFgDuAGLN1z00rPbxAAAAAElFTkSuQmCC"',
+  ],
+];
 
 const _alignCookiesToEnv = (envGetter) => (r) => {
   const env = envGetter(r);
-
   let cookies = env
     ? [`__CMSENV=${env === "cin3" ? "default" : env}; path=/`]
     : ["__CMSENV=deleted; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT"];
@@ -97,30 +93,7 @@ const _alignCookiesToEnv = (envGetter) => (r) => {
   );
 };
 
-const cmsMenuBarFilters = (r, data, flags) => {
-  data = _addAppButtons(data);
-  _replaceCmsDomains("host")(r, data, flags);
-};
-
 export default {
-  proxyDestinationCorsham: _getProxyDestination("UpstreamCmsIpCorsham"),
-  proxyDestinationCorshamInternal: _getProxyDestination("UpstreamCmsIpCorsham"),
-  proxyDestinationModernCorsham: _getProxyDestination(
-    "UpstreamCmsModernIpCorsham"
-  ),
-  proxyDestinationModernCorshamInternal: _getProxyDestination(
-    "UpstreamCmsModernIpCorsham"
-  ),
-  proxyDestinationFarnborough: _getProxyDestination("UpstreamCmsIpFarnborough"),
-  proxyDestinationFarnboroughInternal: _getProxyDestination(
-    "UpstreamCmsIpFarnborough"
-  ),
-  proxyDestinationModernFarnborough: _getProxyDestination(
-    "UpstreamCmsModernIpFarnborough"
-  ),
-  proxyDestinationModernFarnboroughInternal: _getProxyDestination(
-    "UpstreamCmsModernIpFarnborough"
-  ),
   upstreamCmsDomainName: _getUpstreamVariable("UpstreamCmsDomainName"),
   upstreamCmsModernDomainName: _getUpstreamVariable(
     "UpstreamCmsModernDomainName"
@@ -136,10 +109,15 @@ export default {
   upstreamCmsModernIpFarnborough: _getUpstreamVariable(
     "UpstreamCmsModernIpFarnborough"
   ),
-  replaceCmsDomains: _replaceCmsDomains("host"),
-  replaceCmsDomainsAjaxViewer: _replaceCmsDomains("websiteHostname"),
-  cmsMenuBarFilters,
-  getDomainFromCookie: _getDomainFromCookie,
+
+  replaceCmsDomains: _filterBody([_cmsDomainReplacements("host")]),
+  replaceCmsDomainsAjaxViewer: _filterBody([
+    _cmsDomainReplacements("websiteHostname"),
+  ]),
+  cmsMenuBarFilters: _filterBody([
+    _appButtonReplacements,
+    _cmsDomainReplacements("host"),
+  ]),
 
   switchEnvironment: _alignCookiesToEnv((r) => r.uri.substring(1)),
   switchEnvironmentDevLogin: _alignCookiesToEnv(
