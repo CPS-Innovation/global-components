2f3f410e02d28cef3b472cad5d0a7841c36f84f7
stef <stef@stachow.co.uk>
2026-03-02 10:35:47 +0000

    #FCT2-14926 - allow recent cases list length to be configurable (#706)

--- packages/cps-global-components/src/services/state/recent-cases/initialise-recent-cases.ts
+++ packages/cps-global-components/src/services/state/recent-cases/initialise-recent-cases.ts
@@ -6,22 +6,17 @@ import { StatePutResponseSchema } from "../StatePutResponse";
 import { RecentCase, RecentCasesSchema } from "./recent-cases";
 import { Register } from "../../../store/store";
 
-export const initialiseRecentCases = async ({
-  rootUrl,
-  register,
-  config,
-}: {
-  rootUrl: string;
-  register: Register;
-  config: Config;
-}) => {
+export const initialiseRecentCases = async ({ rootUrl, register, config }: { rootUrl: string; register: Register; config: Config }) => {
   const { RECENT_CASES_LIST_LENGTH } = config;
 
   if (!RECENT_CASES_LIST_LENGTH) {
     return { setNextRecentCases: () => {} };
   }
 
-  const recentCasesPromise = fetchState({ rootUrl, url: "../state/recent-cases", schema: RecentCasesSchema, defaultResultWhenNull: [] });
+  const recentCasesPromise = fetchState({ rootUrl, url: "../state/recent-cases", schema: RecentCasesSchema, defaultResultWhenNull: [] })
+    // edge case: if we have reduced the RECENT_CASES_LIST_LENGTH setting and the user has accumulated a list longer than
+    //  the new setting lets make sure the setting is respected immediately.
+    .then(result => (result.found ? { ...result, result: result.result.slice(0, RECENT_CASES_LIST_LENGTH) } : result));
 
   recentCasesPromise.then(recentCases => {
     register({ recentCases });
