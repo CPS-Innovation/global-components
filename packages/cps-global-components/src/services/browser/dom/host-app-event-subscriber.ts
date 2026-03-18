1c6509fce9c758bd0f9ff238f686ec4420079f8c
stef <stef@stachow.co.uk>
2026-03-18 09:58:36 +0000

    #FCT2-15902 - add POC for host app event listening (#745)

--- packages/cps-global-components/src/services/browser/dom/host-app-event-subscriber.ts
+++ packages/cps-global-components/src/services/browser/dom/host-app-event-subscriber.ts
@@ -6,28 +6,39 @@ const { _debug } = makeConsole("hostAppEventSubscriber");
 
 export const hostAppEventSubscriber: DomMutationObserver = ({ context }) => ({
   isActiveForContext: !!context.hostAppEventTargets?.length,
-  subscriptions: (context.hostAppEventTargets || []).map(target => ({
-    cssSelector: target.selector,
-    handler: (element: Element) => {
-      const dispatch = () =>
-        dispatchHostAppEvent({
-          action: target.action,
-          elementId: target.selector,
-          contextIds: context.contextIds || "",
-        });
+  subscriptions: (context.hostAppEventTargets || []).map(target => {
+    let fired = false;
 
-      if (target.action === "appear") {
-        _debug("Element appeared", target.selector);
-        dispatch();
-      } else {
-        _debug("Attaching click listener to", target.selector);
-        element.addEventListener("click", () => {
-          _debug("Host app event fired for", target.selector);
+    return {
+      cssSelector: target.selector,
+      handler: (element: Element) => {
+        if (fired) return true;
+        fired = true;
+
+        const dispatch = () =>
+          dispatchHostAppEvent({
+            action: target.action,
+            elementId: target.selector,
+            contextIds: context.contextIds || "",
+          });
+
+        if (target.action === "appear") {
+          _debug("Element appeared", target.selector);
           dispatch();
-        }, { passive: true, once: true });
-      }
+        } else {
+          _debug("Attaching click listener to", target.selector);
+          element.addEventListener(
+            "click",
+            () => {
+              _debug("Host app event fired for", target.selector);
+              dispatch();
+            },
+            { passive: true, once: true },
+          );
+        }
 
-      return true;
-    },
-  })),
+        return true;
+      },
+    };
+  }),
 });
