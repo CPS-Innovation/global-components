faac73e1ae545b761aea1cc215350f37dd56aa4f
stef <stef@stachow.co.uk>
2026-03-31 14:27:52 +0100

    #FCT2-16418 - fall back to authHint in no auth in feature flags (#775)

--- packages/cps-global-components/src/services/data/case-details-subscription-factory.ts
+++ packages/cps-global-components/src/services/data/case-details-subscription-factory.ts
@@ -4,67 +4,93 @@ import { SubscriptionFactory } from "../../store/subscriptions/SubscriptionFacto
 import { Handover } from "../state/handover/Handover";
 import { fetchAndValidate } from "../fetch/fetch-and-validate";
 import { MonitoringCodesSchema } from "./MonitoringCode";
+import { CaseIdentifiers } from "../context/CaseIdentifiers";
 
 type Props = {
   setNextHandover: (data: Handover) => void;
   setNextRecentCases: (caseDetails: CaseDetails | undefined) => void;
-  fetch: typeof fetch;
+  fetch?: typeof fetch;
   showMonitoringCodes: boolean;
 };
 
+const tryHandleEmptyCaseIdentifiers = (
+  caseIdentifiers: CaseIdentifiers | undefined,
+  { register }: Pick<Parameters<SubscriptionFactory>[0], "register">,
+) => {
+  if (caseIdentifiers) return false;
+  register({ caseDetails: undefined, caseMonitoringCodes: undefined });
+  return true;
+};
+
+const tryHandleCaseDetailsFromHandover = (
+  caseIdentifiers: CaseIdentifiers,
+  { register, mergeTags, get }: Pick<Parameters<SubscriptionFactory>[0], "register" | "mergeTags" | "get">,
+) => {
+  const caseId = Number(caseIdentifiers.caseId);
+  const handover = get("handover");
+  const handoverForCase = handover?.found && handover.result.caseId === caseId ? handover.result : undefined;
+  if (!handoverForCase?.caseDetails) return false;
+
+  mergeTags({ caseDetailsTags: extractTagsFromCaseDetails(handoverForCase.caseDetails).tags });
+  register({ caseDetails: { found: true, result: handoverForCase.caseDetails } });
+  if (handoverForCase.monitoringCodes) {
+    register({ caseMonitoringCodes: { found: true, result: handoverForCase.monitoringCodes } });
+  }
+  return true;
+};
+
+const tryHandleDisabledDataAccess = (fetch: typeof globalThis.fetch | undefined) => !fetch;
+
+const tryRetrieveCaseDetails = (
+  caseIdentifiers: CaseIdentifiers,
+  fetch: typeof globalThis.fetch,
+  { showMonitoringCodes, setNextHandover, setNextRecentCases }: Props,
+  { register, mergeTags }: Pick<Parameters<SubscriptionFactory>[0], "register" | "mergeTags">,
+) => {
+  const caseId = Number(caseIdentifiers.caseId);
+  // Keep the calls and their registering separate as one may be slower than the other and the UI
+  // may be able to render useful stuff with whatever it has first
+  const caseDetailsPromise = fetchAndValidate(fetch, `/api/global-components/cases/${caseId}/summary`, CaseDetailsSchema)
+    .then(caseDetails => {
+      mergeTags({ caseDetailsTags: extractTagsFromCaseDetails(caseDetails).tags });
+      register({ caseDetails: { found: true, result: caseDetails } });
+      return caseDetails;
+    })
+    .catch(error => {
+      register({ caseDetails: { found: false, error } });
+      return undefined;
+    });
+
+  const monitoringCodesPromise = !showMonitoringCodes
+    ? Promise.resolve(undefined)
+    : fetchAndValidate(fetch, `/api/global-components/cases/${caseId}/monitoring-codes?assignedOnly=true`, MonitoringCodesSchema)
+      .then(caseMonitoringCodes => {
+        register({ caseMonitoringCodes: { found: true, result: caseMonitoringCodes } });
+        return caseMonitoringCodes;
+      })
+      .catch(error => {
+        register({ caseMonitoringCodes: { found: false, error } });
+        return undefined;
+      });
+
+  Promise.all([caseDetailsPromise, monitoringCodesPromise]).then(([caseDetails, monitoringCodes]) => {
+    const handover = { caseId, caseDetails, monitoringCodes };
+    setNextHandover(handover);
+    register({ handover: { found: true, result: handover } });
+    setNextRecentCases(caseDetails);
+  });
+};
+
 export const caseDetailsSubscriptionFactory =
-  ({ fetch, setNextHandover, setNextRecentCases, showMonitoringCodes }: Props): SubscriptionFactory =>
-  ({ register, mergeTags, get }) => ({
+  (props: Props): SubscriptionFactory =>
+  (args) => ({
     type: "onChange",
     handler: {
       propName: "caseIdentifiers",
-      handler: caseIdentifiers => {
-        if (!caseIdentifiers) {
-          register({ caseDetails: undefined, caseMonitoringCodes: undefined });
-          return;
-        }
-        const caseId = Number(caseIdentifiers.caseId);
-        const handover = get("handover");
-        // If we have this case's details handed over then we do not have to hit the apis
-        const { caseDetails, monitoringCodes } = (handover?.found && handover.result.caseId === caseId && handover.result) || {};
-
-        // Let's keep the calls and their registering separate as one may be slower than the other and the UI
-        // may be able to render useful stuff with whatever it has first
-        const caseDetailsPromise = (caseDetails ? Promise.resolve(caseDetails) : fetchAndValidate(fetch, `/api/global-components/cases/${caseId}/summary`, CaseDetailsSchema))
-          .then(caseDetails => {
-            mergeTags({ caseDetailsTags: extractTagsFromCaseDetails(caseDetails).tags });
-            register({ caseDetails: { found: true, result: caseDetails } });
-            return caseDetails;
-          })
-          .catch(error => {
-            register({ caseDetails: { found: false, error } });
-            return undefined;
-          });
-
-        const monitoringCodesPromise = !showMonitoringCodes
-          ? Promise.resolve(undefined)
-          : (
-            monitoringCodes
-              ? Promise.resolve(monitoringCodes)
-              : fetchAndValidate(fetch, `/api/global-components/cases/${caseId}/monitoring-codes?assignedOnly=true`, MonitoringCodesSchema)
-          )
-            .then(caseMonitoringCodes => {
-              register({ caseMonitoringCodes: { found: true, result: caseMonitoringCodes } });
-              return caseMonitoringCodes;
-            })
-            .catch(error => {
-              register({ caseMonitoringCodes: { found: false, error } });
-              return undefined;
-            });
-
-        Promise.all([caseDetailsPromise, monitoringCodesPromise]).then(([caseDetails, monitoringCodes]) => {
-          const handover = { caseId, caseDetails, monitoringCodes };
-          // Send this to the state endpoint...
-          setNextHandover(handover);
-          // ... and update the store (it might be the case that we did not have a handover to begin with)
-          register({ handover: { found: true, result: handover } });
-          setNextRecentCases(caseDetails);
-        });
-      },
+      handler: caseIdentifiers =>
+        tryHandleEmptyCaseIdentifiers(caseIdentifiers, args)
+          || tryHandleCaseDetailsFromHandover(caseIdentifiers!, args)
+          || tryHandleDisabledDataAccess(props.fetch)
+          || tryRetrieveCaseDetails(caseIdentifiers!, props.fetch!, props, args),
     },
   });
