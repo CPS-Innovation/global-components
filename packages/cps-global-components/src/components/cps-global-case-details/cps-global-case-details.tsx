f27997a29495de2efccf889c38ae1de7ff319917
stef <stef@stachow.co.uk>
2026-06-16 15:54:03 +0100

    #FCT2-12203 - watch for 403 on case details (#987)

--- packages/cps-global-components/src/components/cps-global-case-details/cps-global-case-details.tsx
+++ packages/cps-global-components/src/components/cps-global-case-details/cps-global-case-details.tsx
@@ -2,6 +2,7 @@ import { Component, h } from "@stencil/core";
 import { FEATURE_FLAGS } from "cps-global-configuration";
 import { readyState } from "../../store/store";
 import { getCaseDefendantHeadline } from "../../services/data/get-case-defendant-headline";
+import { httpStatus } from "../../services/fetch/fetch-and-validate";
 
 @Component({
   tag: "cps-global-case-details",
@@ -18,6 +19,13 @@ export class CpsGlobalCaseDetails {
     if (!caseIdentifiers?.caseId) {
       // We are not on a case-specific page, or we do not have caseId yet
       return null;
+    }
+
+    // A 403 from the case-details endpoint means the user is not entitled to view this
+    // case. First-pass treatment: keep the row (so the menu layout and dividers are
+    // unaffected) but hide its content. UI/UX will advise on the final experience.
+    if (caseDetails?.found === false && httpStatus(caseDetails.error) === 403) {
+      return <div class="level case-details"></div>;
     }
 
     // If there is a urn in our tags then use that while case details are being obtained
